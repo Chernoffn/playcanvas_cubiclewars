2ba43e37632e84c6b763424e0d5e176fbeecf254
Eric Smith <paytonrules@gmail.com>
2015-02-17 19:25:59 -0600

    Stuff

--- camera.js
+++ camera.js
@@ -10,7 +10,7 @@ pc.script.create("scrolling camera", function(context) {
 
     onMouseMove: function(event) {
       if (event.x < 10) {
-        this.entity.setPosition(this.entity.getPosition().x = this.entity.getPosition.x - 0.5, this.entity.getPosition().y);
+        this.entity.setPosition(this.entity.getPosition.x - 0.5, this.entity.getPosition().y);
       }
     }
 
