aae13aa09cb68bf0a36d223af316fc3862046dc6
Eric Smith <paytonrules@gmail.com>
2015-02-20 18:06:30 -0600

    Refactor helper to encapsulate more
    This should simplify the tests by createing an app once and a context.

--- spec/camera_spec.js
+++ spec/camera_spec.js
@@ -2,22 +2,11 @@ describe("Camera", function() {
   var expect = chai.expect;
 
   it("does some stuff with the context", function() {
-    var canvas = document.createElement("canvas");
-    var app = new pc.Application(canvas, {});
-
-    var context = {};
-    pc.extend(context, function() {
-      var mouse = new pc.Mouse();
-      return {
-        mouse: mouse
-      };
-    }());
-
     var entity = new pc.Entity();
-    var ScrollingCamera = pc.scripts["scrolling camera"](context);
+    var ScrollingCamera = test.getConstructor("scrolling camera");
 
     new ScrollingCamera(entity);
-    context.mouse.fire(pc.EVENT_MOUSE_MOVE, {x: 5});
+    test.context.mouse.fire(pc.EVENT_MOUSE_MOVE, {x: 5});
 
     expect(true).to.be.true;
   });
--- spec/helper.js
+++ spec/helper.js
@@ -1,4 +1,21 @@
-pc.scripts = {};
-pc.script.on("created", function(a, b) {
-  pc.scripts[a] = b;
-});
+test = (function() {
+  var canvas = document.createElement("canvas");
+  var app = new pc.Application(canvas, {});
+  var mouse = new pc.Mouse(canvas);
+  var context = {
+    mouse: mouse
+  };
+
+  var scripts = {};
+  pc.script.on("created", function(componentName, initializer) {
+    scripts[componentName] = initializer;
+  });
+
+  return {
+    canvas: canvas,
+    context: context,
+    getConstructor: function(name) {
+      return scripts[name](context);
+    }
+  };
+})();
