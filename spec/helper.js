765b0b2e9dad58f32e24f24c239eea2624050b69
Eric Smith <paytonrules@gmail.com>
2015-03-08 19:23:12 +0000

    Make the helper inherit from app

--- spec/helper.js
+++ spec/helper.js
@@ -1,20 +1,18 @@
-if (typeof(window.test) === "undefined") {
-  window.test = (function() {
+if (typeof(window.app) === "undefined") {
+  window.app = (function() {
     var canvas = document.createElement("canvas");
     var app = new pc.Application(canvas, {});
-    app.mouse = new pc.Mouse(canvas);
-
+  
     var scripts = {};
     pc.script.on("created", function(componentName, initializer) {
       scripts[componentName] = initializer;
     });
-
-    return {
-      canvas: canvas,
-      app: app,
-      getConstructor: function(name) {
-        return scripts[name](app);
-      }
+    app.mouse = new pc.Mouse(canvas);
+    var testApp = Object.create(app);
+    testApp.canvas = canvas;
+    testApp.getConstructor = function(name) {
+      return scripts[name](app);
     };
-  })();
+    return testApp
+  }());
 }
